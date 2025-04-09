57227f18db36b2a8cc2ff6c3b4645e49eb0f94b9
Nívia Maria <niviamcosta@yahoo.com.br>
2025-04-09 15:07:17 -0300

    Sexto commit
    Finalizando o projeto com js e jquery

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,9 @@
 //efeito de esconder formulário de cadastro
+
+$(document).ready(function(){//O jquery é uma biblioteca que visa minimizar o código do javaScript. Esse é um comando padrão para iniciar o jquery. Veja que existe uma função dentro do 'ready'.
+    $("botao-cadastrar").click(function(){//Este código do jquery minimiza o seguinte código do javaScript: document.getElementById("botao-cadastrar").click()
+        $("#form-cadastrar").slideToggle("slow"); //Neste código do jquery, o 'Toggle' funcionará da seguinte forma: se o formulário estiver escondido (hide), ele irá ser mostrado (show); e se estiver sendo mostrado, o formuário vai ficar encondido.
+        $("#section-login").slideToggle("slow");
+        $("#botao-cadastrar").hide(); //Neste código do jquery, o 'hide' funcionará da seguinte forma: o formulário deverá ser escondido (hide).
+    });
+}); 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 //efeito do botão voltar ao Topo
 
+function topo(){
+    window.scrollTo ( //como o 'scrollTo' é uma matriz, foi necessário abrir e fechar as chaves dentro do parêntesis que o acompanha.
+        {
+            top: 0,
+            left: 0,
+            behavior: "smooth" //o comportamento do elemento (do botão Voltar ao Topo) é ser mais lento.
+        }
+    ) //A função 'window' é para pegar a tela do navegador; o 'scrollTo' é o método que serve para a barra de rolagem do navegador.
+}
+
 //Validação de Login
 
+function login(){ //Aplico uma função para o botão do 'login'.
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value; //O 'getElementById' é usado para selecionar o elemento que tem o 'id' chamado de 'usuário', sendo que busco pelo valor do 'usuário'.
+    var senha = document.getElementById("senha").value; //Para garantir, faço o mesmo comando para o elemento que tem o 'id' chamado 'senha.
+
+    if(usuario == 'Nivia' && senha == '123456'){ //Uso essa condição para validar e, além disso, eu poderia definir outro valor para usuário e para a senha. 
+        window.location = 'index.html'; //quando clicado, o botão vai para a página 'index.html'. 
+        logado = 1; //quando logado, o elemento vai de zero (valor definido anteriormente) para um. 
+    }
+
+    if(logado == 0){ //Em caso de quando logado permanecer no valor zero em vez de um, mostrar mensagem de alerta com o dizer a seguir: 
+        alert("Acesso Negado! Dados incorretos!");
+    }
+}
 //Ativar alert no botão cadastrar
+
+function cadastro(){
+    alert("Cadastro com sucesso!");
+    window.location.href = "index.html";
+}
